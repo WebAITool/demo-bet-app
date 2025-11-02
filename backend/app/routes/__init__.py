a72ff949ff6f317628dc6e8f8cda9251c8bb8481
Bogdan Svirsky <svirskybogdan@yandex.ru>
2025-11-02 22:24:35 +0700

    feat/1: init user endpoints

--- backend/app/routes/__init__.py
+++ backend/app/routes/__init__.py
@@ -1,5 +1,6 @@
 from .auth import router as auth_router
 from .events import router as events_router
 from .bets import router as bets_router
+from .user import router as user_router
 
-__all__ = ["auth_router", "events_router", "bets_router"]
+__all__ = ["auth_router", "events_router", "bets_router", "user_router"]
